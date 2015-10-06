4bb7599299154d6769ad1f5b9567628378480b9b
you12724 <id-12724.s.v@ruri.waseda.jp>
2015-10-06 23:25:13 +0900

    Initial Commit

---
+++ Popupview-swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Popupview-swift
+//
+//  Created by you on 2015/10/06.
+//  Copyright © 2015年 you. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Popupview-swiftTests/Popupview_swiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  Popupview_swiftTests.swift
+//  Popupview-swiftTests
+//
+//  Created by you on 2015/10/06.
+//  Copyright © 2015年 you. All rights reserved.
+//
+
+import XCTest
+@testable import Popupview_swift
+
+class Popupview_swiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
